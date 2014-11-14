9ede8922202d63fbc71aa9db0a58c42800ee56bc
Ahmed ALY <ahmed.aly@student.tgm.ac.at>
2014-11-14 10:47:29 +0100

    updated triggers

--- sql/buch_triggers.sql
+++ sql/buch_triggers.sql
@@ -2,65 +2,76 @@
 -- Buch (MySQL)
 
 --autor
-DLIMITER //
-CREATE OR REPLACE TRIGGER insert_autor AFTER INSERT ON autor
+DELIMITER //
+--DROP TRIGGER insert_autor;
+CREATE TRIGGER insert_autor AFTER INSERT ON autor
 	FOR EACH ROW BEGIN
-    	INSERT INTO autor_changes (id, name, gebdat, time_stamp, action) VALUES (:NEW.id, :NEW.name, :NEW.gebdat, NOW(), "INSERT");
+    	INSERT INTO autor_changes (id, name, gebdat, time_stamp, action) VALUES (NEW.id, NEW.name, NEW.gebdat, NOW(), "INSERT");
     END; //
-DELMITER ;
+DELIMITER ;
 
-DLIMITER //
-CREATE OR REPLACE TRIGGER delete_autor AFTER DELETE ON autor
+DELIMITER //
+----DROP TRIGGER delete_autor;
+CREATE TRIGGER delete_autor AFTER DELETE ON autor
 	FOR EACH ROW BEGIN
-    	INSERT INTO autor_changes (id, name, gebdat, time_stamp, action) VALUES (:OLD.id, :OLD.name, :OLD.gebdat, NOW(), "DELETE");
+    	INSERT INTO autor_changes (id, name, gebdat, time_stamp, action) VALUES (OLD.id, OLD.name, OLD.gebdat, NOW(), "DELETE");
     END; //
-DELMITER ;
+DELIMITER ;
 
-DLIMITER //
-CREATE OR REPLACE TRIGGER update_autor AFTER UPDATE ON autor
+DELIMITER //
+--DROP TRIGGER update_autor;
+CREATE TRIGGER update_autor AFTER UPDATE ON autor
 	FOR EACH ROW BEGIN
-    	INSERT INTO autor_changes (id, name, gebdat, time_stamp, action) VALUES (:OLD.id, :OLD.name, :OLD.gebdat, NOW(), "UPDATE");
+    	INSERT INTO autor_changes (id, name, gebdat, time_stamp, action) VALUES (NEW.id, NEW.name, NEW.gebdat, NOW(), "UPDATE");
     END; //
-DELMITER ;
+DELIMITER ;
 
 --verlag
-CREATE OR REPLACE TRIGGER insert_verlag AFTER INSERT ON verlag
+DELIMITER //
+--DROP TRIGGER insert_verlag;
+CREATE TRIGGER insert_verlag AFTER INSERT ON verlag
 	FOR EACH ROW BEGIN
-    	INSERT INTO verlag_changes (id, name, time_stamp, action) VALUES (:NEW.id, :NEW.name, NOW(), "INSERT");
+    	INSERT INTO verlag_changes (id, name, time_stamp, action) VALUES (NEW.id, NEW.name, NOW(), "INSERT");
     END; //
-DELMITER ;
+DELIMITER ;
 
-DLIMITER //
-CREATE OR REPLACE TRIGGER delete_verlag AFTER DELETE ON verlag
+DELIMITER //
+--DROP TRIGGER delete_verlag;
+CREATE TRIGGER delete_verlag AFTER DELETE ON verlag
 	FOR EACH ROW BEGIN
-    	INSERT INTO verlag_changes (id, name, time_stamp, action) VALUES (:OLD.id, :OLD.name, NOW(), "DELETE");
+    	INSERT INTO verlag_changes (id, name, time_stamp, action) VALUES (OLD.id, OLD.name, NOW(), "DELETE");
     END; //
-DELMITER ;
+DELIMITER ;
 
-DLIMITER //
-CREATE OR REPLACE TRIGGER update_verlag AFTER UPDATE ON verlag
+DELIMITER //
+--DROP TRIGGER update_verlag;
+CREATE TRIGGER update_verlag AFTER UPDATE ON verlag
 	FOR EACH ROW BEGIN
-    	INSERT INTO verlag_changes (id, name, time_stamp, action) VALUES (:OLD.id, :OLD.name, NOW(), "UPDATE");
+    	INSERT INTO verlag_changes (id, name, time_stamp, action) VALUES (NEW.id, NEW.name, NOW(), "UPDATE");
     END; //
-DELMITER ;
+DELIMITER ;
 
 --buch
-CREATE OR REPLACE TRIGGER insert_buch AFTER INSERT ON verlag
+DELIMITER //
+--DROP TRIGGER insert_buch;
+CREATE TRIGGER insert_buch AFTER INSERT ON verlag
 	FOR EACH ROW BEGIN
-    	INSERT INTO verlag_changes (isbn, titel, autor_id, verlag_id, time_stamp, action) VALUES (:NEW.isbn, :NEW.titel, :NEW.autor_id, :NEW.verlag_id, NOW(), "INSERT");
+    	INSERT INTO verlag_changes (isbn, titel, autor_id, verlag_id, time_stamp, action) VALUES (NEW.isbn, NEW.titel, NEW.autor_id, NEW.verlag_id, NOW(), "INSERT");
     END; //
-DELMITER ;
+DELIMITER ;
 
-DLIMITER //
-CREATE OR REPLACE TRIGGER delete_buch AFTER DELETE ON verlag
+DELIMITER //
+--DROP TRIGGER delete_buch;
+CREATE TRIGGER delete_buch AFTER DELETE ON verlag
 	FOR EACH ROW BEGIN
-    	INSERT INTO verlag_changes (isbn, titel, autor_id, verlag_id, time_stamp, action) VALUES (:OLD.isbn, :OLD.titel, :OLD.autor_id, :OLD.verlag_id, NOW(), "DELETE");
+    	INSERT INTO verlag_changes (isbn, titel, autor_id, verlag_id, time_stamp, action) VALUES (OLD.isbn, OLD.titel, OLD.autor_id, OLD.verlag_id, NOW(), "DELETE");
     END; //
-DELMITER ;
+DELIMITER ;
 
-DLIMITER //
-CREATE OR REPLACE TRIGGER update_buch AFTER UPDATE ON verlag
+DELIMITER //
+--DROP TRIGGER update_buch;
+CREATE TRIGGER update_buch AFTER UPDATE ON verlag
 	FOR EACH ROW BEGIN
-    	INSERT INTO (isbn, titel, autor_id, verlag_id, time_stamp, action) VALUES (:OLD.isbn, :OLD.titel, :OLD.autor_id, :OLD.verlag_id, NOW(), "UPDATE");
+    	INSERT INTO verlag_changes (isbn, titel, autor_id, verlag_id, time_stamp, action) VALUES (NEW.isbn, NEW.titel, NEW.autor_id, NEW.verlag_id, NOW(), "UPDATE");
     END; //
-DELMITER ;
+DELIMITER ;
