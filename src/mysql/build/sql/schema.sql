1e83164fe6cd76eabd1ca09e4943aac4b650c917
Ahmed ALY <ahmed.aly@student.tgm.ac.at>
2014-11-14 09:11:54 +0100

    bug fix in schema.xml uodated sql added

--- src/mysql/build/sql/schema.sql
+++ src/mysql/build/sql/schema.sql
@@ -47,5 +47,52 @@ CREATE TABLE `buch`
     INDEX `buch_FI_2` (`verlag_id`)
 ) ENGINE=MyISAM;
 
+-- ---------------------------------------------------------------------
+-- autor_changes
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `autor_changes`;
+
+CREATE TABLE `autor_changes`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `gebdat` DATE NOT NULL,
+    `time_stamp` DATE NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- verlag_changes
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `verlag_changes`;
+
+CREATE TABLE `verlag_changes`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `time_stamp` DATE NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- buch_changes
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `buch_changes`;
+
+CREATE TABLE `buch_changes`
+(
+    `isbn` VARCHAR(24) NOT NULL,
+    `titel` VARCHAR(50) NOT NULL,
+    `autor_id` INTEGER,
+    `verlag_id` INTEGER,
+    `time_stamp` DATE NOT NULL,
+    PRIMARY KEY (`isbn`),
+    INDEX `buch_changes_FI_1` (`autor_id`),
+    INDEX `buch_changes_FI_2` (`verlag_id`)
+) ENGINE=MyISAM;
+
 # This restores the fkey checks, after having unset them earlier
 SET FOREIGN_KEY_CHECKS = 1;
--- src/psql/build/sql/schema.sql
+++ src/psql/build/sql/schema.sql
@@ -28,6 +28,41 @@ CREATE TABLE "werk"
     PRIMARY KEY ("name")
 );
 
+-----------------------------------------------------------------------
+-- autor_changes
+-----------------------------------------------------------------------
+
+DROP TABLE IF EXISTS "autor_changes" CASCADE;
+
+CREATE TABLE "autor_changes"
+(
+    "id" serial NOT NULL,
+    "gebdat" VARCHAR(50) NOT NULL,
+    "beschreibung" VARCHAR,
+    "time_stamp" DATE NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+-----------------------------------------------------------------------
+-- werk_changes
+-----------------------------------------------------------------------
+
+DROP TABLE IF EXISTS "werk_changes" CASCADE;
+
+CREATE TABLE "werk_changes"
+(
+    "name" VARCHAR NOT NULL,
+    "autor_id" INTEGER,
+    "isbn" VARCHAR(24) NOT NULL,
+    "release" DATE NOT NULL,
+    "time_stamp" DATE NOT NULL,
+    PRIMARY KEY ("name")
+);
+
 ALTER TABLE "werk" ADD CONSTRAINT "werk_FK_1"
+    FOREIGN KEY ("autor_id")
+    REFERENCES "autor" ("id");
+
+ALTER TABLE "werk_changes" ADD CONSTRAINT "werk_changes_FK_1"
     FOREIGN KEY ("autor_id")
     REFERENCES "autor" ("id");
